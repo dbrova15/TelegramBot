a785d8a51004df50999a4c1ad84a40ba66f17a44
alexbeatnik <alexbeatnik@gmail.com>
2019-08-29 16:37:11 +0300

    Add travis check Add black check in travis after pull request

---
+++ code-assessment.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+
+#!/usr/bin/env bash
+
+declare -a FAILURES
+
+add_fail() {
+    FAILURES+=("$1")
+}
+
+black --check . || add_fail black
+if [[ ${#FAILURES[@]} -ne 0 ]]; then
+    cat <<RESULT
+===================================================
+= Code assessment is failed! Please fix errors!!! =
+===================================================
+Failed tool(s):
+RESULT
+    for var in "${FAILURES[@]}"; do
+        echo "- ${var}"
+    done
+    exit 11
+fi
